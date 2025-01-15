35c2eba5c685781b6fffe18d3c1cdf0d4afb97d7
Yamcha142005 <yamkallachamak@gmail.com>
2025-01-15 18:00:49 +0100

    Create const_dec.h

---
+++ src/const_dec.h
@@ -0,0 +1,12 @@
+#ifndef CONST_H
+    #define CONST_H
+    
+    #define MAX_PER_REQUEST 1024
+    #define MAX_WAITING_USER 10
+    #define PORT 1111
+    #define FAMILY AF_INET
+    #define SOCK_TYPE SOCK_STREAM
+    #define MAX_FILE_CONTENT_SIZE 16192
+    #define MAX_LEN_FOR_URL 150
+    #define VIEWS_DIR "../views"
+#endif
