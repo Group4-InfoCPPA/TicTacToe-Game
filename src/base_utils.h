c42ef7579e3569cc95e8198960692f198288f9e5
Saobane11111 <saobane060@gmail.com>
2025-01-15 17:54:19 +0100

    Create base_utils.h

---
+++ src/base_utils.h
@@ -0,0 +1,10 @@
+#ifndef BASE_URL_H
+    #define BASE_URL_H
+    #include <stdlib.h>
+    #include <string.h>
+    char digit_to_char(int n);
+    void reverse(char tab[]);
+    int int_to_char(int n, char int_[]);
+    int long_to_char(long long n, char int_[]);
+    char *replace(char *before, char *oldsub, char *newsub);
+#endif
